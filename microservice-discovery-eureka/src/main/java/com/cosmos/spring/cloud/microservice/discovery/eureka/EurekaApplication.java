f1c17dd0d3a67b1ad2aebab2f4da5d0d9c627cad
dingchengjie <dingchengjie01@fangdd.com>
2017-03-13 18:44:13 +0800

    add eureka register center

---
+++ microservice-discovery-eureka/src/main/java/com/cosmos/spring/cloud/microservice/discovery/eureka/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.cosmos.spring.cloud.microservice.discovery.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Eureka服务发现
+ *
+ * @author david
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
