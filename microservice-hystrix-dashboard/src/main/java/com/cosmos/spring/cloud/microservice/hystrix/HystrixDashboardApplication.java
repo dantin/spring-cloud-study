6de8174b72bb621e5d654bfdb72d953f9886aa0c
dingchengjie <dingchengjie01@fangdd.com>
2017-03-14 10:10:06 +0800

    add hystrix dashboard

---
+++ microservice-hystrix-dashboard/src/main/java/com/cosmos/spring/cloud/microservice/hystrix/HystrixDashboardApplication.java
@@ -0,0 +1,17 @@
+package com.cosmos.spring.cloud.microservice.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * Created by david on 13/03/2017.
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(HystrixDashboardApplication.class).web(true).run(args);
+    }
+}
